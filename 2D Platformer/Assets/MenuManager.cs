1897c0ec5b774d22c4802e691aa5190bb0d3c639
nickdee1991 <34762234+nickdee1991@users.noreply.github.com>
2017-12-29 23:50:44 +1300

    EP24 complete
    added custom start menu and game over screen
    (is actually a playable game now)

---
+++ 2D Platformer/Assets/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public void StartGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("WE QUIT THIS SHIT");
+        Application.Quit();
+    }
+}
