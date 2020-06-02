29dd2e27c90d8f259227a2014ece5880c2e96ec2
Nick <34762234+nickdee1991@users.noreply.github.com>
2020-06-02 20:21:38 +1200

    End of day 2 for NotesOfCitrus
    *finished all assets needed and imported them into unity
    *added prefab rooms for proc gen
    *added lighting for tests
    *added player movement for testing
    *minor fixes to some assets in blender

---
+++ NotesOfCitrus/Assets/Scripts/PlayerSimpleMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSimpleMovement : MonoBehaviour
+{
+    public int movementSpeed = 1;
+    public int rotationSpeed = 5;
+
+    private void Start()
+    {
+
+    }
+    public void Update()
+    {
+        #region Player Movement
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * -movementSpeed * rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * movementSpeed * rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+        }
+        #endregion End Player Movement
+    }
+}
